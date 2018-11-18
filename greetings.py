c77bcdb1d08c668e6055da321a92448bbaaa853c
Armastus <david.o.bustillos@gmail.com>
2018-11-17 18:18:35 -0800

    bleep

---
+++ greetings.py
@@ -0,0 +1,14 @@
+import turtle
+
+dizzy = turtle
+
+# name = input("What's your name?")
+# print("Hello,", name, "!")
+print("Nice to meet you!")
+
+print("Hello world!")
+
+def say_hello():
+    return "Hello!"
+
+say_hello()
